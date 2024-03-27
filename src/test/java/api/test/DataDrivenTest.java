0baef1eb3aed767ae9a7e79db3bae78b87b6d74f
RahulK9495 <rahulkhot5554@gmail.com>
2024-03-27 19:57:01 +0530

    Second commit

--- src/test/java/api/test/DataDrivenTest.java
+++ src/test/java/api/test/DataDrivenTest.java
@@ -10,11 +10,10 @@ import io.restassured.response.Response;
 
 public class DataDrivenTest {
 
-	
-	@Test(priority=1, dataProvider="Data", dataProviderClass=dataProviders.class)
-	public void createUserByPost(String userID, String userName, String fname, String lname, String email,String pwd, String ph)
-	{
-		User user=new User();
+	@Test(priority = 1, dataProvider = "Data", dataProviderClass = dataProviders.class)
+	public void createUserByPost(String userID, String userName, String fname, String lname, String email, String pwd,
+			String ph) {
+		User user = new User();
 		user.setId(Integer.parseInt(userID));
 		user.setUsername(userName);
 		user.setFirstName(fname);
@@ -22,15 +21,14 @@ public class DataDrivenTest {
 		user.setEmail(email);
 		user.setPassword(pwd);
 		user.setPhone(ph);
-		
-		Response response=UserEndpoints.createUser(user);
+
+		Response response = UserEndpoints.createUser(user);
 		Assert.assertEquals(response.getStatusCode(), 200);
 	}
-	
-	@Test(priority=2, dataProvider="UserNames", dataProviderClass=dataProviders.class)
-	public void deleteUser(String username)
-	{
-		Response response=UserEndpoints.deleteUser(username);
+
+	@Test(priority = 2, dataProvider = "UserNames", dataProviderClass = dataProviders.class)
+	public void deleteUser(String username) {
+		Response response = UserEndpoints.deleteUser(username);
 		Assert.assertEquals(response.getStatusCode(), 200);
 	}
 }
